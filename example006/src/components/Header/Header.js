c4fc6d4ec944d449543b7756145b9d184d148e4f
Mats Wikmar <matwik@gmail.com>
2017-10-22 11:36:29 +0200

    react-router-v4-start branch v1

--- example006/src/components/Header/Header.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Header = (props) => <h2>{props.text}</h2> ;
-
-export default Header;
--- example013/src/actions/actionTypes.js
+++
@@ -1,9 +0,0 @@
-export const GET_TODOS        = 'GET_TODOS';
-export const POST_TODO        = 'POST_TODO';
-export const REMOVE_TODO      = 'REMOVE_TODO';
-export const UPDATE_TODO      = 'UPDATE_TODO';
-export const FETCH_ERROR      = 'FETCH_ERROR';
-export const UPDATE_LOADING   = 'UPDATE_LOADING';
-export const SIGN_IN          = 'SIGN_IN';
-export const SIGN_OUT         = 'SIGN_OUT';
-
